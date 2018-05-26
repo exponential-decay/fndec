5c6e72cd9036af05ae37f121a9e08a9e29ed7471
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2018-05-26 19:30:57 +0200

    Add usage output and cross-compile build cmd

---
+++ fnbuild.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eux
+
+MOON="fndec"
+DIR="release"
+mkdir "$DIR"
+env GOOS=windows GOARCH=386 go build
+mv "$MOON".exe "${DIR}/${MOON}"-win386.exe
+env GOOS=windows GOARCH=amd64 go build
+mv "$MOON".exe "${DIR}/${MOON}"-win64.exe
+env GOOS=linux GOARCH=amd64 go build
+mv "$MOON" "${DIR}/${MOON}"-linux64
+env GOOS=darwin GOARCH=386 go build
+mv "$MOON" "${DIR}/${MOON}"-darwin386
+env GOOS=darwin GOARCH=amd64 go build
+mv "$MOON" "${DIR}/${MOON}"-darwinAmd64
--- fndec.go
+++ fndec.go
@@ -8,12 +8,16 @@ import (
 	"path/filepath"
 )
 
+const version = "fndec-0.0.2"
+
 var (
 	file string
+	vers bool
 )
 
 func init() {
-	flag.StringVar(&file, "file", "false", "File to read the fname from.")
+	flag.StringVar(&file, "file", "false", "File to read the filename from.")
+	flag.BoolVar(&vers, "version", false, "Return version information.")
 }
 
 func readFileName(path string, fi os.FileInfo, err error) error {
@@ -24,5 +28,17 @@ func readFileName(path string, fi os.FileInfo, err error) error {
 
 func main() {
 	flag.Parse()
+	if vers {
+		fmt.Fprintf(os.Stderr, "%s\n", version)
+		os.Exit(0)
+	} else if flag.NFlag() < 1 {
+		fmt.Fprintln(os.Stderr, "Usage: fndec [-file ...]")
+		fmt.Fprintln(os.Stderr, "             [Optional: -version]")
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, "Output: Character encoding for fname: {fname} detected: {encoding}")
+		fmt.Fprintf(os.Stderr, "Output: [String] '%s ...'\n\n", version)
+		flag.Usage()
+		os.Exit(0)
+	}
 	filepath.Walk(file, readFileName)
 }
